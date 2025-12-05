ece596bb442948a84ed2e5d2a75ca127e3c31f08
Mdforidulislam <mdforidulislam962@gmail.com>
2025-12-05 21:28:08 +0600

    added:add new code space

--- app/page.tsx
+++ app/page.tsx
@@ -12,6 +12,9 @@ export default function Home() {
           height={20}
           priority
         />
+
+
+        
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
             To get started, edit the page.tsx file.
@@ -23,7 +26,7 @@ export default function Home() {
               className="font-medium text-zinc-950 dark:text-zinc-50"
             >       
 
-            
+
               Templates
             </a>{" "}
             or the{" "}
